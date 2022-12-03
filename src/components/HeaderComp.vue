b1293e4290c60236f54b41a484d3da2424b7b22b
NikiRufolo <nikirufolo@gmail.com>
2022-12-03 19:10:07 +0100

    mading second header

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -16,16 +16,19 @@
   <h6>As long as poverty, injustice & inequality persist, none of us can truly rest</h6>
   <button>Our mission</button> <button class="btn2">Donate now</button>
 </div>
-<div class="div-white">
-  <div><h3>EVERY MOMENT COUNTS</h3></div>
-  <h6>Lorem, Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus explicabo, a placeat quisquam nihil dignissimos quas odio, magni reprehenderit illum delectus incidunt libero? Nesciunt dolorem iusto quae necessitatibus! Officiis, iste? consectetur adipisicing elit. Magnam quia praesentium, ducimus similique autem eveniet Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora quibusdam recusandae voluptatibus consequatur odit sunt, earum laboriosam at Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias, consectetur maxime aperiam quas, explicabo ratione totam exercitationem,  Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate enim, hic saepe aut tempore quas quo, maxime mollitia aperiam quos fugiat Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur nam, aliquam odit quasi optio consequatur debitis dolore unde mollitia excepturi Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo, odio beatae adipisci cupiditate, hic voluptatibus fuga ducimus obcaecati at assumenda dignissimos aliquid eum consectetur in aliquam praesentium error mollitia est. reprehenderit nemo ut, magni perspiciatis possimus aperiam, impedit nisi iusto. necessitatibus, ducimus culpa illo voluptatum ex recusandae deserunt. Inventore. nostrum vero fugiat et saepe? Nobis sit, dolor nam dolore rem voluptas maiores. distinctio aspernatur quae, ipsum repellendus molestiae vero eaque, debitis a officia veniam. iste minus itaque fuga aut a rem incidunt ipsum nesciunt odit, vel laboriosam nobis maiores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel doloribus quidem recusandae pariatur. Ducimus explicabo temporibus perferendis voluptatum obcaecati, Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, quidem animi exercitationem nulla ducimus aut similique sint in dolore, suscipit molestiae tenetur ea ratione doloremque quae placeat unde vero sit? soluta, praesentium corrupti iste sequi sunt asperiores, harum nisi expedita. Possimus!  ipsum dolor sit amet consectetur adipisicing elit. Maiores ratione est eos rerum harum dicta commodi libero incidunt tenetur, praesentium, ducimus inventore laudantium itaque veniam nemo hic quod sequi alias? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus vitae aperiam doloremque at maiores quasi, perferendis, fugit repudiandae reiciendis quas placeat labore voluptates iste facilis in error eos tenetur officiis!</h6>
-</div>
+<SecondHeader/>
     </header>
 </template>
 
 <script>
+import SecondHeader from './SecondHeader.vue'
+
+
 export default {
   name: 'HeaderComp',
+  components: {
+    SecondHeader
+  },
   props: {
     propsNav: Array
   }
@@ -35,9 +38,9 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
-.header-nav{
-  // background-color: yellow;
-}
+// .header-nav{
+//   background-color: yellow;
+// }
 
 .div-flex {
 display: flex;
@@ -70,6 +73,7 @@ width: 375px;
 margin-top:300px;
 text-align: center;
 color: white;
+padding-bottom: 100px;
 }
 button { 
         display: inline-block;
@@ -100,10 +104,20 @@ button {
         border: 2px double#e4de78f0;
         color: #e4de78f0;
       }
-      .div-white {
-      background-color: white;
-      width: 65%;
-      // margin-top: 400px;
-      // margin-right: 500px;
-      }
+
+//       @mixin flex($justify) {
+//     display: flex;
+
+//     @if $justify == 'Both_Center'{
+//         justify-content: center;
+//         align-items: center;
+//     }
+//     @else if $justify == 'Between_Center'{
+//         justify-content: space-between;
+//         align-items: center;
+//     }
+//     @else if $justify == 'Between'{
+//         justify-content: space-between;
+//     }
+// }
 </style>
---
+++ src/components/SecondHeader.vue
@@ -0,0 +1,97 @@
+<template>
+    <main>
+        <div class="div-white">
+            <div>
+                <h2>EVERY MOMENT COUNTS</h2> <br>
+                <hr> <br>
+
+                <h6>Doloribus, esse. Corporis numquam hic ducimus quibusdam eligendi voluptates perspiciatis placeat
+                    perferendis, labore officiis laboriosam quos. Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+                    consectetur adipisicing elite <br> <br>
+                    Lorem, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, esse. Corporis
+                    necessitatibus enim dignissimos consequuntur eveniet assumenda numquam hic ducimus quibusdam
+                    eligendi
+                    voluptates perspiciatis placeat perferendis, labore officiis laboriosam quos. Lorem ipsum dolor sit
+                    amet
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit <br> <br>
+                    Lorem, Lorem ipsum elitet assumenda numquam hic ducimus quibusdam eligendi voluptates perspiciatis
+                    placeat perferendis, labore officiis laboriosam quos. Lorem ipsum dolor sit amet </h6>
+                    <br>
+                    <button>OUR MISSION</button>
+            </div>
+
+
+        </div>
+        <div class="div-red">
+            <h6>Lorem, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, esse. Corporis
+                necessitatibus enim dignissimos consequuntur eveniet assumenda numquam hic ducimus quibusdam eligendi
+                voluptates perspiciatis placeat perferendis, labore officiis laboriosam quos. Lorem ipsum dolor sit amet
+                Lorem ipsum dolor sit amet consectetur adipisicing elit</h6>
+            <h6>Lorem, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, esse. Corporis
+                necessitatibus enim dignissimos consequuntur eveniet assumenda numquam hic ducimus quibusdam eligendi
+                voluptates perspiciatis placeat perferendis, labore officiis laboriosam quos. Lorem ipsum dolor sit amet
+                Lorem ipsum dolor sit amet consectetur adipisicing elit</h6>
+            <h6>Lorem, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, esse. Corporis
+                necessitatibus enim dignissimos consequuntur eveniet assumenda numquam hic ducimus quibusdam eligendi
+                voluptates perspiciatis placeat perferendis, labore officiis laboriosam quos. Lorem ipsum dolor sit amet
+                Lorem ipsum dolor sit amet consectetur adipisicing elit</h6>
+
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'SecondHeader',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.div-white {
+    background-color: white;
+    width: 65%;
+}
+
+.div-red {
+    background-color: rgb(255, 0, 0);
+    width: 35%;
+}
+
+.div-white>div {
+    padding: 100px;
+}
+
+main {
+    display: flex;
+    margin-top: 300px;
+}
+
+hr {
+    width: 20%;
+    margin-left: 0;
+    border-top: 2px solid #f5cc00;
+}
+
+button {
+    display: inline-block;
+    background-color: #f5cc00;
+    border-radius: 0px;
+    border: 2px double #ffffff;
+    color: #eeeeee;
+    text-align: center;
+    font-size: 15px;
+    padding: 3px;
+    width: 130px;
+    transition: all 0.5s;
+    cursor: pointer;
+    margin: 0 auto;
+}
+
+button:hover {
+    background-color: #f7c2f9;
+}
+</style>
